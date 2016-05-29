849035f8aaa5b77c1d25f6813c9da6c9a6fb811a
anxzhu <anxzhu@qq.com>
2016-05-29 10:38:08 +0800

    up
    arduino library

---
+++ A6seglcd/examples/a6seglcd/a6seglcd/a6seglcd.ino
@@ -0,0 +1,18 @@
+  #include <A6seglcd.h> //库
+  a6seglcd lcd;         //   
+  void setup()
+  {
+  lcd.run(4,5,6,10);   //定义  cs（片选）  write（写） data（数据）   backlight（背光） 这 4个引脚  （vcc  gnd 接arduino的 vcc  gnd）
+  lcd.backlighton();   //开背光（关背光是 backlightoff() ）
+  lcd.conf();          //配置 偏压  输出
+  lcd.clr();           //清屏
+  }
+  void loop()
+  {
+  delay(20);          // 稳定 stable
+  lcd.dispnum(0.123);  //要显示的数值the number you want to diaplay
+  lcd.bat1();         //开电池电量显示,一共三格电 关闭电量显示是 bat1off()   ,for battery display,  same to bat1off()
+  lcd.bat2();         //bat2off()
+  lcd.bat3();         //bat3off()
+   
+  }

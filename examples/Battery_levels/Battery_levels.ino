0cb666935ad9a33ec8c15eb0edffd63e27004e0c
5N44P <valerionappi995@gmail.com>
2018-12-09 21:52:27 +0100

    chaged backlighton and backlightoff to backlight() and noBacklight() in examples, fixes #1

--- examples/Battery_levels/Battery_levels.ino
+++ examples/Battery_levels/Battery_levels.ino
@@ -1,7 +1,7 @@
 /*
   Battery Levels
 
-  Displays the various battery levels with 500ms 
+  Displays the various battery levels with 500ms
   pause between.
 
   The circuit:
@@ -12,7 +12,7 @@
 
   Created 9 dec 2018
   By valerio\new (5N44P)
-  
+
   https://github.com/5N44P/ht1621-7-seg
 
 */
@@ -24,24 +24,24 @@ HT1621 lcd; // create an "lcd" object
 
 
 void setup(){
-  // start the lcd: 
+  // start the lcd:
   // cs to pin 13
   // wr to pin 12
   // Data to pin 8
   // backlight to pin 10
   // you can chose whichever pin you want
 
-  
-  lcd.begin(13, 12, 8, 10); // (cs, wr, Data, backlight)  
+
+  lcd.begin(13, 12, 8, 10); // (cs, wr, Data, backlight)
   // if no backlight control is given, you can also use:
   // lcd.begin(13, 12, 8); // (cs, wr, Data)
-  
-  lcd.backlighton(); // turn on the backlight led
-  
+
+  lcd.backlight(); // turn on the backlight led
+
   lcd.clear(); // clear the screen
 }
 
-void loop(){ 
+void loop(){
   lcd.battlevel(1);
   delay(500);
   lcd.battlevel(2);
--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -9,7 +9,7 @@
   backlight to pin 10
   Created 9 dec 2018
   By valerio\new (5N44P)
-  
+
   https://github.com/5N44P/ht1621-7-seg
 */
 
@@ -20,7 +20,7 @@ HT1621 lcd; // create an "lcd" object
 
 
 void setup(){
-  // start the lcd: 
+  // start the lcd:
   // cs to pin 13
   // wr to pin 12
   // Data to pin 8
@@ -28,17 +28,17 @@ void setup(){
   // you can chose whichever pin you want
 
 
-  
-  lcd.begin(13, 12, 8, 10); // (cs, wr, Data, backlight)  
+
+  lcd.begin(13, 12, 8, 10); // (cs, wr, Data, backlight)
   // if no backlight control is given, you can also use:
   // lcd.begin(13, 12, 8); // (cs, wr, Data)
-  
-  lcd.backlighton(); // turn on the backlight led
-  
+
+  lcd.backlight(); // turn on the backlight led
+
   lcd.clear(); // clear the screen
 }
 
-void loop(){ 
+void loop(){
   lcd.print(millis()/1000.0, 3); // print the floating point seconds value with 3 decimal digits precision
   delay(1); // wait 1 millisecond
 }
